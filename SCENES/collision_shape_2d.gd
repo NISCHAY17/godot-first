d0f84e790929c4ec5f0c0a878cf593d38c951c2f
NISCHAY KUMAR <NischayTech@gmail.com>
2026-01-15 14:30:38 +0530

    WORKING ON ENEMY

---
+++ SCENES/collision_shape_2d.gd
@@ -0,0 +1,9 @@
+extends CollisionShape2D
+
+const Speed = 60 
+var direction = 1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += Speed * delta 
+	
+	
