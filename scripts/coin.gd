02a4337f67c7b9d5bbda04684f61f8225107f938
NISCHAY KUMAR <NischayTech@gmail.com>
2026-01-15 18:05:31 +0530

    AHH EVEN FRAUD CANT SAVE ME NOW

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,9 +2,10 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
+@onready var gamemanager: Node = %gamemanager
 
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+ 1 COIN")
+	gamemanager.add_point()
 	queue_free()
