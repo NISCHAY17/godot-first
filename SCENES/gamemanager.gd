01e93f116d6b1b8512ddb33bce1626606f0b0fd0
NISCHAY KUMAR <NischayTech@gmail.com>
2026-01-15 19:06:33 +0530

    AUDIO GO BR

--- SCENES/gamemanager.gd
+++ SCENES/gamemanager.gd
@@ -1,7 +1,9 @@
 extends Node
 
 var score = 0
+@onready var coinscore: Label = $COINSCORE
 
 func add_point():
 	score += 1
 	print(score)
+	coinscore.text = "You Collected" + str(score) + " coins "
