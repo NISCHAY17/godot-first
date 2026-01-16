0ed036d8e94d59dfe990372897503401407a0e57
NISCHAY KUMAR <NischayTech@gmail.com>
2026-01-16 19:38:00 +0530

    tryna lock in

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,9 +3,11 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 @onready var gamemanager: Node = %gamemanager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
 func _on_body_entered(body: Node2D) -> void:
 	gamemanager.add_point()
+	animation_player.play("COINPICK")
 	queue_free()
