d809233016615f59f9fe0989357aa21ef9c36f19
NISCHAY KUMAR <NischayTech@gmail.com>
2026-01-18 15:46:52 +0530

    i fked the tilemaps wow

--- SCENES/player.gd
+++ SCENES/player.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 190.0
-const JUMP_VELOCITY = -250.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,27 +16,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	
-	
-	
-	var direction := Input.get_axis("moveleft", "move right")
-	if direction > 0:
-		animated_sprite.flip_h = false
-	
-	
-	
-	if direction < 1:
-		animated_sprite.flip_h = true
-	
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("IDLE")
-		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-			
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
